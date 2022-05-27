d041548f93ea4b6b8178c47f227e9c651d60f3dd
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-05-27 23:06:13 +0200

    Inserting Openstreetmaps into the application.

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 import "./Map.css"
+import {MapContainer, Marker, TileLayer} from "react-leaflet";
+import'leaflet/dist/leaflet.css';
+import '../../utils/fix-map-icon';
 
 export const Map = ()=> {
   return (
     <div className="map">
-      ...
+      <MapContainer center={[50.0297138,22.0203527]} zoom={13} >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution="https://www.openstreetmap.org/copyright"
+        />
+        <Marker position={[50.0297138,22.0203527]}>
+          <h2>Rzeszów</h2>
+        </Marker>
+      </MapContainer>
     </div>
   )
 }
